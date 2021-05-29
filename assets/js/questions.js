e97dd10813dc27461c3070e30eaa827389b692eb
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-05-28 17:52:23 -0700

    created questions in questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,96 @@
+// List of questions
+var questions = {
+  0: {
+    question: "What tag define the body inside the HTML page?",
+    a: "<!DOCTYPE html>",
+    b: "<body>",
+    c: "<header>",
+    d: "HTML",
+    win: "b",
+  },
+
+  1: {
+    question: "Why would you use and ID over a Class?",
+    a: "ID's can be used multiple of times",
+    b: "ID's are more cool to use",
+    c: "ID is used once, while Class can be used multiple of times",
+    d: "Class is better to use",
+    win: "c",
+  },
+
+  2: {
+    question: "How do you close a tag?",
+    a: "$",
+    b: "<img src=>",
+    c: "</ >",
+    d: ";",
+    win: "c",
+  },
+
+  3: {
+    question: "What tag is used to underline a word or line of text?",
+    a: "<li>",
+    b: "<ul>",
+    c: "<s>",
+    d: "<u>",
+    win: "d",
+  },
+
+  4: {
+    question: "What tag is used to define an unordered list that is bulleted?",
+    a: "<ul>",
+    b: "<u>",
+    c: "<li>",
+    d: "<s>",
+    win: "c",
+  },
+
+  5: {
+    question:
+      "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+    a: "Restriction",
+    b: "Output",
+    c: "Range",
+    d: "Scope",
+    win: "d",
+  },
+
+  6: {
+    question:
+      "What is considered to be the most popular programming language in the world?",
+    a: "HTML",
+    b: "CSS",
+    c: "Node",
+    d: "JavaScript",
+    win: "d",
+  },
+
+  7: {
+    question:
+      "In JavaScript, what is a block of code called that is used to perform a specific task?",
+    a: "Variable",
+    b: "Array",
+    c: "String",
+    d: "Function",
+    win: "d",
+  },
+
+  8: {
+    question:
+      "What is the name of the property that is used by writing down your logic?",
+    a: "Pseudocode",
+    b: "Syntax",
+    c: "Summary",
+    d: "Aligment",
+    win: "a",
+  },
+
+  9: {
+    question: "What is JavaScript?",
+    a: "HTML",
+    b: "Coffee while reading",
+    c: "Programming or scripting language",
+    d: "A link providing errors via dubugging",
+    win: "c",
+  },
+};
